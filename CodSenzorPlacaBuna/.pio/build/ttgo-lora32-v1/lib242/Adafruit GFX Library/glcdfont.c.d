ae3278d96e9d638f4b17032b482b60ffadaa8e98
djtibiii <117279489+djtibi@users.noreply.github.com>
2025-01-09 20:48:50 +0100

    ????wtf

---
+++ CodSenzorPlacaBuna/.pio/build/ttgo-lora32-v1/lib242/Adafruit GFX Library/glcdfont.c.d
@@ -0,0 +1,2 @@
+.pio/build/ttgo-lora32-v1/lib242/Adafruit\ GFX\ Library/glcdfont.c.o: \
+ .pio/libdeps/ttgo-lora32-v1/Adafruit\ GFX\ Library/glcdfont.c
---
+++ Sensor FIrst Test/.pio/build/ttgo-lora32-v1/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/ttgo-lora32-v1/FrameworkArduino/USBMSC.cpp.o: \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
