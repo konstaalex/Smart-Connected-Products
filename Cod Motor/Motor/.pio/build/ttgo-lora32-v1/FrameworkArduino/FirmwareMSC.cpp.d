ceeb0a5bb53b5d1094953fd1c4a4eac401bcf92c
djtibiii <117279489+djtibi@users.noreply.github.com>
2025-01-19 17:54:40 +0100

    ??

---
+++ Cod Motor/Motor/.pio/build/ttgo-lora32-v1/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/ttgo-lora32-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
