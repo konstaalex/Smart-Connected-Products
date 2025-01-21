55cb47a300921a7b2907accb3db0cfcfdb895b48
djtibiii <117279489+djtibi@users.noreply.github.com>
2025-01-21 13:56:23 +0100

    Motor

---
+++ Motor Running When Humidity Raises copy/Motor2.0/.pio/build/ttgo-lora32-v1/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/ttgo-lora32-v1/FrameworkArduino/firmware_msc_fat.c.o: \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
---
+++ Motor Running When Humidity Raises copy/Motor2.0/.pio/build/ttgo-lora32-v1/lib93c/Adafruit GFX Library/glcdfont.c.d
@@ -0,0 +1,2 @@
+.pio/build/ttgo-lora32-v1/lib93c/Adafruit\ GFX\ Library/glcdfont.c.o: \
+ .pio/libdeps/ttgo-lora32-v1/Adafruit\ GFX\ Library/glcdfont.c
