b85c124e63dfb1fa9dc8d48c62b02c730287a2e7
konstaalex <a.constantinescu@student.fontys.nl>
2025-01-21 14:56:22 +0100

    updated github

--- Sensor FIrst Test/.pio/build/ttgo-lora32-v1/FrameworkArduino/USBCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/ttgo-lora32-v1/FrameworkArduino/USBCDC.cpp.o: \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
