e5146d4132530c6da3524b18f9356b34b6d05c0f
djtibiii <117279489+djtibi@users.noreply.github.com>
2025-01-21 13:56:02 +0100

    .

---
+++ Motor Running When Humidity Raises/Motor2.0/.pio/build/ttgo-lora32-v1/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,12 @@
+.pio/build/ttgo-lora32-v1/FrameworkArduino/stdlib_noniso.c.o: \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
