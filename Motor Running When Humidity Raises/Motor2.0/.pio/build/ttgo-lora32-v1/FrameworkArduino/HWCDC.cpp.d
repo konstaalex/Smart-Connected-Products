c21f939012686e7bc74456db1e1faccf924dc988
djtibiii <117279489+djtibi@users.noreply.github.com>
2025-01-22 16:12:12 +0100

    Delete Motor Running When Humidity Raises/Motor2.0 directory

--- Motor Running When Humidity Raises/Motor2.0/.pio/build/ttgo-lora32-v1/FrameworkArduino/HWCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/ttgo-lora32-v1/FrameworkArduino/HWCDC.cpp.o: \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- Motor Running When Humidity Raises/Motor2.0/.pio/build/ttgo-lora32-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/ttgo-lora32-v1/FrameworkArduino/cbuf.cpp.o: \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/djtibiii/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
